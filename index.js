31dd481a0407347f3d271780262436297a6d898c
Robert Reinhard <robert.reinhard@bukwild.com>
2018-04-26 15:22:17 -0700

    Switching back to webpack
    Had trouble getting rollup working right, particularly with babel and cjs

--- index.js
+++ index.js
@@ -1,43 +1,172 @@
-'use strict';
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
 
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var once = _interopDefault(require('lodash/once'));
-var contentful = _interopDefault(require('contentful'));
-var queryString = _interopDefault(require('query-string'));
-var isObject = _interopDefault(require('lodash/isObject'));
-var merge = _interopDefault(require('lodash/merge'));
-var defaults = _interopDefault(require('lodash/defaults'));
+var makeClient, merge, queries;
 
-function createCommonjsModule(fn, module) {
-	return module = { exports: {} }, fn(module, module.exports), module.exports;
-}
+merge = __webpack_require__(1);
+
+// We will export an object
+module.exports = {};
+
+// Accept the API configuration and create the client instance
+makeClient = __webpack_require__(2);
+
+module.exports.client = {}; // Needed for the client to be added later
+
+module.exports.config = function (options) {
+  return merge(module.exports, {
+    client: makeClient(options)
+  });
+};
+
+// Add image helper
+module.exports.image = __webpack_require__(5);
+
+// Add seo helper
+module.exports.seo = __webpack_require__(7);
+
+// Add querying helpers
+queries = __webpack_require__(9);
+
+module.exports.getEntries = queries.getEntries;
+
+module.exports.getPaginatedEntries = queries.getPaginatedEntries;
+
+module.exports.getEntry = queries.getEntry;
+
+module.exports.getEntryBySlug = queries.getEntryBySlug;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("lodash/merge");
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
 
 // Build the contentful client
-var contentful$1, once$1;
+var contentful, once;
 
-once$1 = once;
+once = __webpack_require__(3);
 
-contentful$1 = contentful;
+contentful = __webpack_require__(4);
 
-var clientFactory = once$1(function (options) {
-  return contentful$1.createClient({
+module.exports = once(function (options) {
+  return contentful.createClient({
     space: options.space,
     accessToken: options.access_token,
     host: options.host || 'cdn.contentful.com'
   });
 });
 
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = require("lodash/once");
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = require("contentful");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
 /*
 Util to make formatting images easier
 */
-var queryString$1;
+var queryString;
 
-queryString$1 = queryString;
+queryString = __webpack_require__(6);
 
-var image = function image(field, width, height) {
+module.exports = function (field, width, height) {
   var options = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
 
   var params, ref, ref1, url;
@@ -56,27 +185,40 @@ var image = function image(field, width, height) {
   }
   // Make the URL
   url = field.fields.file.url;
-  return url + '?' + queryString$1.stringify(params);
+  return url + '?' + queryString.stringify(params);
 };
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("query-string");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
 
 /*
 A helper for making the Nuxt head from a contentful SEO entry
 */
-var isObject$1, merge$1;
+var isObject, merge;
 
-isObject$1 = isObject;
+isObject = __webpack_require__(8);
 
-merge$1 = merge;
+merge = __webpack_require__(1);
 
-var seo = function seo() {
+module.exports = function () {
   var seo = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-  var defaults$$1 = arguments[1];
+  var defaults = arguments[1];
 
   var ref;
   // Merge seo reference on the entry (which may be absent) with an object of
   // default values pulled from an entry.
-  seo = merge$1({}, defaults$$1, seo.fields || {});
-  if ((seo != null ? seo.image : void 0) && isObject$1(seo.image)) {
+  seo = merge({}, defaults, seo.fields || {});
+  if ((seo != null ? seo.image : void 0) && isObject(seo.image)) {
     // Get the image URL from raw image fields
     seo.image = seo.image.fields.file.url;
   }
@@ -122,212 +264,107 @@ var seo = function seo() {
   };
 };
 
-var asyncToGenerator = function (fn) {
-  return function () {
-    var gen = fn.apply(this, arguments);
-    return new Promise(function (resolve, reject) {
-      function step(key, arg) {
-        try {
-          var info = gen[key](arg);
-          var value = info.value;
-        } catch (error) {
-          reject(error);
-          return;
-        }
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
 
-        if (info.done) {
-          resolve(value);
-        } else {
-          return Promise.resolve(value).then(function (value) {
-            step("next", value);
-          }, function (err) {
-            step("throw", err);
-          });
-        }
-      }
+module.exports = require("lodash/isObject");
 
-      return step("next");
-    });
-  };
+/***/ }),
+/* 9 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/*
+Helpers to build reusuable queries
+*/
+var defaults, getClient, merge;
+
+// Deps
+defaults = __webpack_require__(10);
+
+merge = __webpack_require__(1);
+
+getClient = __webpack_require__(2);
+
+// Gonna export an object
+module.exports = {};
+
+// Get a list of entries given a content type
+module.exports.getEntries = function (contentType) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+  var client;
+  client = getClient();
+  return client.getEntries(defaults(options, {
+    content_type: contentType
+  }));
 };
 
-var queries = createCommonjsModule(function (module) {
-  /*
-  Helpers to build reusuable queries
-  */
-  var defaults$$1, getClient;
+// Get a paginateed list of entries, supporting a different number of results
+// on the first page.
+module.exports.getPaginatedEntries = function (contentType, _ref) {
+  var _ref$page = _ref.page,
+      page = _ref$page === undefined ? 1 : _ref$page,
+      _ref$perPage = _ref.perPage,
+      perPage = _ref$perPage === undefined ? 12 : _ref$perPage,
+      _ref$initialPerPage = _ref.initialPerPage,
+      initialPerPage = _ref$initialPerPage === undefined ? null : _ref$initialPerPage;
+  var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
 
-  // Deps
-  defaults$$1 = defaults;
+  var query;
+  if (!initialPerPage) {
+    // Set default initial per apge
+    initialPerPage = perPage;
+  }
+  // Form pagination query
+  query = defaults(options, {
+    skip: page === 1 ? 0 : (page - 2) * perPage + initialPerPage,
+    limit: page === 1 ? initialPerPage : perPage
+  });
+  // Run the query
+  return module.exports.getEntries(contentType, query);
+};
 
-  getClient = clientFactory;
+// Get a single item
+module.exports.getEntry = function (contentType) {
+  var query = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-  // Gonna export an object
-  module.exports = {};
-
-  // Get a list of entries given a content type
-  module.exports.getEntries = function (contentType) {
-    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-
-    var client;
-    client = getClient();
-    return client.getEntries(defaults$$1(options, {
-      content_type: contentType
-    }));
-  };
-
-  // Get a paginateed list of entries, supporting a different number of results
-  // on the first page.
-  module.exports.getPaginatedEntries = function (contentType, _ref) {
-    var _ref$page = _ref.page,
-        page = _ref$page === undefined ? 1 : _ref$page,
-        _ref$perPage = _ref.perPage,
-        perPage = _ref$perPage === undefined ? 12 : _ref$perPage,
-        _ref$initialPerPage = _ref.initialPerPage,
-        initialPerPage = _ref$initialPerPage === undefined ? null : _ref$initialPerPage;
-    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-
-    var query;
-    if (!initialPerPage) {
-      // Set default initial per apge
-      initialPerPage = perPage;
+  var client;
+  client = getClient();
+  return client.getEntries(defaults(query, {
+    content_type: contentType,
+    limit: 1
+  })).then(function (entry) {
+    var fields;
+    if (!entry.items.length) {
+      return;
     }
-    // Form pagination query
-    query = defaults$$1(options, {
-      skip: page === 1 ? 0 : (page - 2) * perPage + initialPerPage,
-      limit: page === 1 ? initialPerPage : perPage
-    });
-    // Run the query
-    return module.exports.getEntries(contentType, query);
-  };
+    // Merge some sys fields into the object and return just the fields
+    fields = entry.items[0].fields;
+    fields.id = entry.items[0].sys.id;
+    fields.createdAt = entry.items[0].sys.createdAt;
+    fields.updatedAt = entry.items[0].sys.updatedAt;
+    return fields;
+  });
+};
 
-  // Get a single item
-  module.exports.getEntry = function () {
-    var _ref2 = asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(contentType) {
-      var query = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-      var client, entry, fields;
-      return regeneratorRuntime.wrap(function _callee$(_context) {
-        while (1) {
-          switch (_context.prev = _context.next) {
-            case 0:
-              client = getClient();
-              _context.next = 3;
-              return client.getEntries(defaults$$1(query, {
-                content_type: contentType,
-                limit: 1
-              }));
+// Get an entry by slug
+module.exports.getEntryBySlug = function (contentType, slug) {
+  var query = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
 
-            case 3:
-              entry = _context.sent;
+  return module.exports.getEntry(contentType, {
+    'fields.slug': slug
+  });
+};
 
-              if (!entry.items.length) {
-                _context.next = 10;
-                break;
-              }
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
 
-              fields = entry.items[0].fields;
-              fields.id = entry.items[0].sys.id;
-              fields.createdAt = entry.items[0].sys.createdAt;
-              fields.updatedAt = entry.items[0].sys.updatedAt;
-              return _context.abrupt('return', fields);
+module.exports = require("lodash/defaults");
 
-            case 10:
-            case 'end':
-              return _context.stop();
-          }
-        }
-      }, _callee, this);
-    }));
-
-    return function (_x4) {
-      return _ref2.apply(this, arguments);
-    };
-  }();
-
-  // Get an entry by slug
-  module.exports.getEntryBySlug = function () {
-    var _ref3 = asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(contentType, slug) {
-      return regeneratorRuntime.wrap(function _callee2$(_context2) {
-        while (1) {
-          switch (_context2.prev = _context2.next) {
-            case 0:
-              _context2.next = 2;
-              return module.exports.getEntry(contentType, {
-                'fields.slug': slug
-              });
-
-            case 2:
-              return _context2.abrupt('return', _context2.sent);
-
-            case 3:
-            case 'end':
-              return _context2.stop();
-          }
-        }
-      }, _callee2, this);
-    }));
-
-    return function (_x6, _x7) {
-      return _ref3.apply(this, arguments);
-    };
-  }();
-});
-var queries_1 = queries.getEntries;
-var queries_2 = queries.getPaginatedEntries;
-var queries_3 = queries.getEntry;
-var queries_4 = queries.getEntryBySlug;
-
-var bukwildContentfulUtils = createCommonjsModule(function (module) {
-  var makeClient, merge$$1, queries$$1;
-
-  merge$$1 = merge;
-
-  // We will export an object
-  module.exports = {};
-
-  // Accept the API configuration and create the client instance
-  makeClient = clientFactory;
-
-  module.exports.client = {}; // Needed for the client to be added later
-
-  module.exports.config = function (options) {
-    return merge$$1(module.exports, {
-      client: makeClient(options)
-    });
-  };
-
-  // Add image helper
-  module.exports.image = image;
-
-  // Add seo helper
-  module.exports.seo = seo;
-
-  // Add querying helpers
-  queries$$1 = queries;
-
-  module.exports.getEntries = queries$$1.getEntries;
-
-  module.exports.getPaginatedEntries = queries$$1.getPaginatedEntries;
-
-  module.exports.getEntry = queries$$1.getEntry;
-
-  module.exports.getEntryBySlug = queries$$1.getEntryBySlug;
-});
-var bukwildContentfulUtils_1 = bukwildContentfulUtils.client;
-var bukwildContentfulUtils_2 = bukwildContentfulUtils.config;
-var bukwildContentfulUtils_3 = bukwildContentfulUtils.image;
-var bukwildContentfulUtils_4 = bukwildContentfulUtils.seo;
-var bukwildContentfulUtils_5 = bukwildContentfulUtils.getEntries;
-var bukwildContentfulUtils_6 = bukwildContentfulUtils.getPaginatedEntries;
-var bukwildContentfulUtils_7 = bukwildContentfulUtils.getEntry;
-var bukwildContentfulUtils_8 = bukwildContentfulUtils.getEntryBySlug;
-
-exports.default = bukwildContentfulUtils;
-exports.client = bukwildContentfulUtils_1;
-exports.config = bukwildContentfulUtils_2;
-exports.image = bukwildContentfulUtils_3;
-exports.seo = bukwildContentfulUtils_4;
-exports.getEntries = bukwildContentfulUtils_5;
-exports.getPaginatedEntries = bukwildContentfulUtils_6;
-exports.getEntry = bukwildContentfulUtils_7;
-exports.getEntryBySlug = bukwildContentfulUtils_8;
+/***/ })
+/******/ ]);

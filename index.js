4497dcd6ecdc07965601fc04bbc6f98b955ef68d
Robert Reinhard <robert.reinhard@bukwild.com>
2018-05-04 08:46:35 -0700

    Make it possible to specify the timeout

--- index.js
+++ index.js
@@ -146,7 +146,8 @@ module.exports = once(function (options) {
   return contentful.createClient({
     space: options.space,
     accessToken: options.access_token,
-    host: options.host || 'cdn.contentful.com'
+    host: options.host || 'cdn.contentful.com',
+    timeout: options.timeout || 30000 // The default
   });
 });
 
--- lib/client-factory.coffee
+++ lib/client-factory.coffee
@@ -5,3 +5,4 @@ module.exports = once (options) -> contentful.createClient
 	space: options.space
 	accessToken: options.access_token
 	host: options.host || 'cdn.contentful.com'
+	timeout: options.timeout || 30000 # The default
